db066eb05c7df426c86a6c84faf9c085aec7a7bb
Gannon2 <111807284+Gannon2@users.noreply.github.com>
2024-10-31 15:13:36 -0400

    Add files via upload

---
+++ SpaceshipsScreen.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { Text, SafeAreaView, FlatList, Button, StyleSheet } from 'react-native';
+
+const SpaceshipsScreen = ({navigation}) => {
+  const teams = ['Placeholder', 'Placeholder', 'Placeholder'];
+   return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Spaceships</Text>
+      <FlatList
+        data={teams}
+        renderItem={({ item }) => <Text style={styles.team}>{item}</Text>}
+        keyExtractor={(item) => item}
+      />
+
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 10
+  },
+  team: {
+    fontSize: 18,
+    marginBottom: 10
+  },
+});
+
+export default SpaceshipsScreen;
